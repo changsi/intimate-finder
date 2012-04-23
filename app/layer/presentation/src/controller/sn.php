feb7db170f5b48d277624347def4260e5a3f21a9
root <root@ubuntu.ubuntu-domain>
2012-04-24 07:56:26 +0800

    sn controller

--- app/layer/presentation/src/controller/sn.php
+++ app/layer/presentation/src/controller/sn.php
@@ -1,7 +1,6 @@
 <?php
 //require getConfigFilePath("config");
 require getConfigFilePath("db_config");
-require getConfigFilePath("sn_config");
 require getLibFilePath("db.driver.MySqlDB");
 require getLibFilePath("util.util");
 
@@ -12,18 +11,18 @@ $live_DB_driver = new MySqlDB();
 $live_DB_driver->connect($DB_CONFIG["HOST"], $DB_CONFIG["DBNAME"], $DB_CONFIG["USERNAME"], $DB_CONFIG["PASSWORD"], $DB_CONFIG["PORT"], false, true);
 $live_DB_driver->setCharset($DB_CONFIG["ENCODING"]);
 ob_clean();
-require getContextFilePath("common.service.IdService");
+//require getContextFilePath("common.service.IdService");
 
 require getContextFilePath("sn.service.SNFacebookService");
-require getContextFilePath("user.service.UserService");
+//require getContextFilePath("user.service.UserService");
 
 
 
 $SNFacebookService = new SNFacebookService();
 $SNFacebookService->setDBDriverForLiveSystem($live_DB_driver);
 
-$UserService = new UserService();
-$UserService->setDBDriverForLiveSystem($live_DB_driver);
+//$UserService = new UserService();
+//$UserService->setDBDriverForLiveSystem($live_DB_driver);
 
 require getPresentationControllerFilePath("default");
 ?>
