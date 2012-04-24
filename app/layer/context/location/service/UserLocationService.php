62f785f41ded4964ecb6353df911ed9c1afee93d
root <root@ubuntu.ubuntu-domain>
2012-04-25 01:19:40 +0800

    1

---
+++ app/layer/context/location/service/UserLocationService.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once getRuleFilePath("facebook_app.UserLocationFacebookAppRule");
+require_once getLibFilePath("io.FileHandler");
+
+class UserLocationService {
+	private $userLocationFacebookAppRule;
+
+
+	public function __construct() {
+		$this->userLocationFacebookAppRule = new UserLocationFacebookAppRule();
+	}
+
+
+	public function setDBDriverForLiveSystem($DBDriver) {
+		$this->userLocationFacebookAppRule->setDBDriver($DBDriver);
+	}
+
+	public function insertUserLocation($data){
+		return $this->userLocationFacebookAppRule->insertUserLocation($data);
+	}
+}
+?>
