72db3cdd076823751abe503b5004368a89f72885
root <root@ubuntu.ubuntu-domain>
2012-04-25 05:49:24 +0800

    1

--- app/layer/context/sn/service/SNFacebookService.php
+++ app/layer/context/sn/service/SNFacebookService.php
@@ -55,33 +55,6 @@ class SNFacebookService {
 		//TODO
 	}
 	
-	//$data = ?
-	//To be used by entity/sn/facebook/logout.php
-	public function logout($data) {
-		$logoutUrl  = $this->facebook->getLogoutUrl();
-		//TODO
-	}
-	
-	/* START GET FUNCTIONS */
-	
-	//$data = [ "user_id" => 123456 , "limit" => 100]
-	//To be used by script/start_fetching_facebook_user_data.php
-	public function getUserFeeds($data) {
-		if($this->user){
-			$user_feed = $this->facebook->api('/'.$data["network_user_id"].'/feed', array("limit" => $data['limit']));
-			return $user_feed;
-		}
-		return false;
-	}
-	
-	//$data = [ "limit" => 100]
-	//To be used by script/start_fetching_facebook_user_data.php
-	public function getUserFriends($data) {
-		if($this->user){
-			$user_friends = $this->facebook->api('/me/friends', array("limit" => $data['limit']));
-			return $user_friends['data'];
-		}
-	}
 	
 	//$data = ["network_user_id" => "45154"]
 	//To be used by script/start_fetching_facebook_user_data.php
@@ -91,13 +64,7 @@ class SNFacebookService {
 			return $user_profile;
 		}
 	}
-	
-	//$data = ?
-	//To be used by script/start_fetching_facebook_user_data.php
-	public function getUserFeedsUrls($data) {
-		//TODO
-		
-	}
+
 	
 	public function getAccessToken() {
 		if($this->user){
@@ -106,29 +73,7 @@ class SNFacebookService {
 		}
 		
 	}
-	
-	//$data = ["network_user_id" => 1232]
-	//get the user name of a user
-	public function getUserName($data) {
-		if($this->user){
-			$result = $this->getUserProfile($data);
-			return $result['name'];
-		}
 		
-	}
-	
-	//$data = ["network_user_id" => 1232]
-	//get the user pseudo (aka username) of a user
-	public function getUserPseudo($data) {
-		if($this->user){
-			$result = $this->getUserProfile($data);
-			if (isset($result['username'])) {
-				return $result['username'];
-			}
-		}
-		return false;
-	}
-	
 	public function getUserId() {
 		if($this->facebook->getUser()){
 			$this->user = $this->facebook->getUser();
@@ -150,92 +95,17 @@ class SNFacebookService {
 		}
 	}
 	
-	//$data = ["user_network_id" => 1234566, "object_type" => "movie"]
 	
-	public function getUserObjectFromFB($data) {
-		switch ($data['object_type']) {
-			case 'movie':
-				return $this->facebook->api('/'.$data["network_user_id"].'/movies');
-			break;
-			case 'book':
-				return $this->facebook->api('/'.$data["network_user_id"].'/books');
-			break;
-			case 'music':
-				return $this->facebook->api('/'.$data["network_user_id"].'/music');
-			break;
-			case 'game':
-				return $this->facebook->api('/'.$data["network_user_id"].'/games');
-			break;
-			case 'television':
-				return $this->facebook->api('/'.$data["network_user_id"].'/television');
-			break;
-			case 'music':
-				return $this->facebook->api('/'.$data["network_user_id"].'/music');
-			break;
-			case 'activity':
-				return $this->facebook->api('/'.$data["network_user_id"].'/activities');
-			break;
-			case 'interest':
-				return $this->facebook->api('/'.$data["network_user_id"].'/interests');
-			break;
-			//TODO
-			//case 'other':
-			//	return $this->facebook->api('/'.$data["network_user_id"].'/?');
-			//break;
-		}
-	}
-	
-	// by si chang
-	public function getUserAndFriendsLikes($data){
+	//$data = [user_id => 1565165]
+	//get the user profile info, checkin and friends list
+	public function getEverythingExceptFriendsCheckin($data) {
 		if($this->user){
 			$queries = array(
-				array('method'=>'GET', 'relative_url'=>'/'.$data['network_user_id'].'/likes'),
-				array('method'=>'GET', 'relative_url'=>'/'.$data['network_user_id'].'/friends',"omit_response_on_success" => false,'name'=>'get-friends'),
-				array('method'=>'GET', 'relative_url'=>'/likes?ids={result=get-friends:$.data.*.id}'),
-				array('method'=>'GET', 'relative_url'=>'fql?q=SELECT pic_big, uid FROM user WHERE uid in({result=get-friends:$.data.*.id})')
-				);
-			return $this->sendBatchRequests($queries,0);
-		}
-	}
-	
-	public function getObjectDetailedInfor($data){
-		$start = $data['start'];
-		$limit = $data['limit'];
-		$ids = $data['data'];
-		$queries = array();
-		for($i=$start ; $i<$start+$limit; $i = $i+20){
-			//echo "\n".implode(',',array_slice($ids,$i, 100))."\n";
-			$queries[] = array('method' => 'GET', 'relative_url' => '?ids='.(implode(',',array_slice($ids,$i, 20))));
-			
-		}
-		
-		return $this->sendBatchRequests($queries,0);
-	}
-	
-	//$data = [network_user_id => 1565165]
-	//get the user profile info feeds and list of friends 
-	public function getEverythingExceptFriendsFeeds($data) {
-		if($this->user){
-			$queries = array(
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id']),
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/feed?limit='.NUMBER_OF_FB_FEEDS_TO_FETCH_LIMIT),
+    				array('method' => 'GET', 'relative_url' => '/'.$data['user_id']),
+    				array('method' => 'GET', 'relative_url' => '/'.$data['user_id'].'/checkins'),
     				
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/movies'),
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/books'),
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/music'),
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/games'),
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/television'),
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/activities'),
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/interests'),
-				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/links?limit='.NUMBER_OF_FB_FEEDS_TO_FETCH_LIMIT),
+    				array('method' => 'GET', 'relative_url' => '/'.$data['user_id'].'/friends', "omit_response_on_success" => false, "name" => "get-friends")
     				
-    				array('method' => 'GET', 'relative_url' => '/'.$data['network_user_id'].'/friends?limit='.NUMBER_OF_FB_FRIENDS_TO_FETCH_LIMIT, "omit_response_on_success" => false, "name" => "get-friends")
-    				// ------ DEPRECATED ------- 
-    				//array('method' => 'GET', 'relative_url' => '?ids={result=get-friends:$.data.*.id}'),
-    				//array('method' => 'GET', 'relative_url' => '/fql?q=SELECT+friend_count+FROM+user+where+uid+=+me()'),
-    				//array('method' => 'GET', 'relative_url' => '/1213475558/feed?limit=1000')
-    				//array('method' => 'GET', 'relative_url' => '/fql?q=SELECT+source_id,message,attachment+FROM+stream+where+source_id+in+(SELECT+uid2+FROM+friend+WHERE+uid1+=+me())+order+by+source_id+limit+0,50'),
-    				//array('method' => 'GET', 'relative_url' => '/fql?q=SELECT+source_id,message,attachment+FROM+stream+where+source_id+in+(SELECT+uid2+FROM+friend+WHERE+uid1+=+me())+order+by+source_id+limit+50,50')
 			);
 			return $this->sendBatchRequests($queries,0);
 		}
@@ -243,7 +113,7 @@ class SNFacebookService {
 	
 	//$data = [start => 0, limit => 5 , friends => array(0 => 341542, 1 => 65443, 2 => 545524.....)]
 	//Function that encapsulates requests to get the profile info and feeds for 5 friends equivalent to 45 requests
-	public function getFriendProfileInfoAndFeeds ($data) {
+	public function getFriendProfileInfoAndCheckin ($data) {
 		if($this->user){
 			
 			$start = $data['start'];
@@ -253,27 +123,12 @@ class SNFacebookService {
 			$queries = array();
 			for ($i = $start; $i < ($start+$limit); $i++) {
 				$queryProfile = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id']);
-				$queryFeeds = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/feed?limit='.NUMBER_OF_FB_FRIENDS_FEEDS_TO_FETCH_LIMIT);
-    				$queryObj1 = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/movies');
-    				$queryObj2 = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/books');
-    				$queryObj3 = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/music');
-    				$queryObj4 = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/games');
-    				$queryObj5 = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/television');
-    				$queryObj6 = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/activities');
-    				$queryObj7 = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/interests');
-    				$queryObj8 = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/links?limit='.NUMBER_OF_FB_FRIENDS_FEEDS_TO_FETCH_LIMIT);
+				$queryCheckin = array('method' => 'GET', 'relative_url' => '/'.$friends[$i]['id'].'/checkins');
+    				
 				
 				
 				$queries[] = $queryProfile;
-				$queries[] = $queryFeeds;
-				$queries[] = $queryObj1;
-				$queries[] = $queryObj2;
-				$queries[] = $queryObj3;
-				$queries[] = $queryObj4;
-				$queries[] = $queryObj5;
-				$queries[] = $queryObj6;
-				$queries[] = $queryObj7;
-				$queries[] = $queryObj8;
+				$queries[] = $queryCheckin;
 			}
 			return $this->sendBatchRequests($queries,0);
 		}
