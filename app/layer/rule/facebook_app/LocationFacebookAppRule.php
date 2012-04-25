a8e17415eb0022c0930cf02de79b56c25d7597e5
root <root@ubuntu.ubuntu-domain>
2012-04-25 11:47:31 +0800

    1

--- app/layer/rule/facebook_app/LocationFacebookAppRule.php
+++ app/layer/rule/facebook_app/LocationFacebookAppRule.php
@@ -6,10 +6,11 @@ class LocationFacebookAppRule extends Rule {
 	public function insertLocation($data) {
 		$created_date = $this->getCurrentTimestamp();	//get current timestamp
 
-		$sql = "insert into location (location_id, name, latitude, longitude, picture_url, address, description) values ("  . $data["location_id"] .
-		", '" . $data["name"] . "', ".$data['latitude'].", ".$data['longitude'].", '".$data['picture_url']."', '".$data['address']."', '".$data['description']
+		$sql = "insert into location (location_id, name, latitude, longitude, picture_url, street,city,state, country, zip, description) values ("  .
+		 $data["location_id"] .", '" . $data["name"] . "', ".$data['latitude'].", ".$data['longitude'].", '".$data['picture_url'].
+		 "', '".$data['street'] ."', '". $data['city'] ."', '". $data['state'] ."', '".$data['country']."', '".$data['zip']."', '". $data['description']
 		."') ON DUPLICATE KEY UPDATE name=VALUES(name), latitude=values(latitude), longitude=values(longitude), picture_url=values(picture_url), 
-		address=values(address), description=values(description)";
+		street=values(street), city=values(city), state=values(state), country=values(country), zip=values(zip),description=values(description)";
 		//echo "\n\n\n". $sql."\n\n\n" ;
 		return $this->setData($sql);
 	}
