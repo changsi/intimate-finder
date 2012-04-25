c9ff9fed645bd772e16869d98e975282cbffaa9f
root <root@ubuntu.ubuntu-domain>
2012-04-25 11:47:17 +0800

    1

--- app/layer/presentation/src/entity/sn/facebook/login.php
+++ app/layer/presentation/src/entity/sn/facebook/login.php
@@ -1,10 +1,5 @@
 <?php
-/*
-Logs in to FaceBook and save the access_token to the DB, so the scripts can run later.
-Additionally starts the script: start_fetching_facebook_user_data.php
-
-Uses: SNFacebookService->login(...)
-*/
+require getLibFilePath('sn.util.facebook_data_wrapper');
 
 $access_token = isset($_SESSION["access_token"]) ? $_SESSION["access_token"] : NULL;
 
@@ -20,12 +15,8 @@ if($user_id) {
 		'user_id'	=>	$user_id
      );
      
-	$user_exist =  $UserService->getUserInfoFromLiveSystem($data);
-	if(!empty($user_exist)){
-		
-	}else{
-		getUserInfo();		
-	}
+	updateUserInfo();
+	
 	echo HOST_PREFIX;
 	echo "<br>";
 	header("Location: ".HOST_PREFIX."/sn/facebook/welcome");
@@ -36,20 +27,11 @@ else{
  	$loginUrl = $SNFacebookService->login();
 }
 
-function getUserInfo() {
+function updateUserInfo() {
 	global $SNFacebookService, $UserService;
 	$user_profile = $SNFacebookService->getUserProfile(array("user_id" => $_SESSION["user_id"]));
-	$birthday= date_parse($user_profile['birthday']);
-	$birth_date = $birthday['year'].'-'.$birthday['month'].'-'.$birthday['day'];
-	$data = array(
-		'access_token'  => $_SESSION['access_token'],
-		'user_id'	=>	$_SESSION["user_id"],
-		'name'	=>	$user_profile['name'],
-		'birth_date' => $birth_date,
-		'gender' => (isset($user_profile['gender'])?2:$user_profile['gender'])=='male'?0:1,
-		'picture_url' => "",
-		'expiry_date' => ''
-     );
+	$data = get_data_from_profile($user_profile);
+	$data['access_token'] = $_SESSION['access_token'];
 
 	$UserService->insertUser($data);
 }
