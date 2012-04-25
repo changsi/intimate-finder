42bbb13f766e307d70c5e198884cdb62ef31d67c
root <root@ubuntu.ubuntu-domain>
2012-04-25 11:46:54 +0800

    1

--- app/layer/presentation/src/controller/sn.php
+++ app/layer/presentation/src/controller/sn.php
@@ -8,7 +8,7 @@ require getLibFilePath("util.util");
 
 //init live system DB driver
 $live_DB_driver = new MySqlDB();
-$live_DB_driver->connect($DB_CONFIG["HOST"], $DB_CONFIG["DBNAME"], $DB_CONFIG["USERNAME"], $DB_CONFIG["PASSWORD"], $DB_CONFIG["PORT"], false, true);
+$live_DB_driver->connect($DB_CONFIG["HOST"], $DB_CONFIG["DBNAME"], $DB_CONFIG["USERNAME"], $DB_CONFIG["PASSWORD"], '', false, true);
 $live_DB_driver->setCharset($DB_CONFIG["ENCODING"]);
 ob_clean();
 //require getContextFilePath("common.service.IdService");
