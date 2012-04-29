1dd8a93cb0bcb12145eb97da28411f098b19574d
Si Chang <changsi0621@gmail.com>
2012-04-29 13:20:51 +0800

    1

--- app/layer/presentation/src/entity/sn/facebook/check_progression.php
+++ app/layer/presentation/src/entity/sn/facebook/check_progression.php
@@ -6,8 +6,6 @@ $user_id = $SNFacebookService->getUserId();
 
 $data = array("user_id"=>$user_id);
 
-touch("/home/changsi/test/test.txt");
-
 $progression = $UserProgressionService->getUserProgress($data);
 
 if(isset($progression[0]) && isset($progression[0]['progress'])){
