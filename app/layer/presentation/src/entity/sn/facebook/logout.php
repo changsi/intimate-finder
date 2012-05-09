dc38167c771ffdb1669e8515bf6a6d8eb1be9bb9
Si Chang <changsi0621@gmail.com>
2012-05-10 03:50:15 +0800

    1

---
+++ app/layer/presentation/src/entity/sn/facebook/logout.php
@@ -0,0 +1,9 @@
+<?php
+
+session_unset();
+session_destroy();
+
+header("Location: ".HOST_PREFIX."/sn/facebook/welcome");
+die();
+
+?>
---
+++ app/layer/presentation/src/entity/sn/facebook/welcome.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = isset($_SESSION["access_token"]) ? $_SESSION["access_token"] : NULL;
+$user_id = isset($_SESSION["user_id"]) ? $_SESSION["user_id"] : NULL;
+
+if($user_id) {
+	$login = true;
+		
+}
+else{
+	$login = false;
+}
+
+
+
+?>
